c748b6057aafbb70f3aa8eb810babf77ef131eb7
tree9990 <zhihao09@gmail.com>
2020-12-29 10:04:12 -0500

    starting tkinter

---
+++ test-ttk.py
@@ -0,0 +1,13 @@
+import tkinter
+from tkinter import messagebox
+
+def Click():
+    replay = messagebox.askquestion ("Quit?", "Are you sure?")
+    if replay == 'yes':
+        skylight.destroy()
+
+skylight = tkinter.Tk()
+skylight.title("Skylight")
+button = tkinter.Button(skylight, text="Bye", command=Click)
+button.place (x=10, y=10)
+skylight.mainloop()
